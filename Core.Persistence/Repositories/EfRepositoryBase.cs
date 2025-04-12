fa3b4e0f747b8a3ae4a50d4373462ec3186f3a9c
aenesyvz <20060403@stu.omu.edu.tr>
2025-04-12 09:47:44 +0300

    Timestamps re-enabled

--- Core.Persistence/Repositories/EfRepositoryBase.cs
+++ Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -25,35 +25,42 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
 
     public async Task<TEntity> AddAsync(TEntity entity)
     {
-       // entity.CreatedDate = DateTime.UtcNow;
+        entity.CreatedDate = DateTime.UtcNow;
+
         await Context.AddAsync(entity);
         await Context.SaveChangesAsync();
+
         return entity;
     }
 
     public async Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entities)
     {
         foreach (TEntity entity in entities)
-          //  entity.CreatedDate = DateTime.UtcNow;
+          entity.CreatedDate = DateTime.UtcNow;
+
         await Context.AddRangeAsync(entities);
         await Context.SaveChangesAsync();
+
         return entities;
     }
 
     public async Task<TEntity> UpdateAsync(TEntity entity)
     {
-       // entity.UpdatedDate = DateTime.UtcNow;
+        entity.UpdatedDate = DateTime.UtcNow;
         Context.Update(entity);
         await Context.SaveChangesAsync();
+
         return entity;
     }
 
     public async Task<ICollection<TEntity>> UpdateRangeAsync(ICollection<TEntity> entities)
     {
         foreach (TEntity entity in entities)
-           // entity.UpdatedDate = DateTime.UtcNow;
+           entity.UpdatedDate = DateTime.UtcNow;
+
         Context.UpdateRange(entities);
         await Context.SaveChangesAsync();
+
         return entities;
     }
 
@@ -61,6 +68,7 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
     {
         await SetEntityAsDeletedAsync(entity, permanent);
         await Context.SaveChangesAsync();
+
         return entity;
     }
 
@@ -68,6 +76,7 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
     {
         await SetEntityAsDeletedAsync(entities, permanent);
         await Context.SaveChangesAsync();
+
         return entities;
     }
 
@@ -156,35 +165,43 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
 
     public TEntity Add(TEntity entity)
     {
-       // entity.CreatedDate = DateTime.UtcNow;
+        entity.CreatedDate = DateTime.UtcNow;
+
         Context.Add(entity);
         Context.SaveChanges();
+
         return entity;
     }
 
     public ICollection<TEntity> AddRange(ICollection<TEntity> entities)
     {
         foreach (TEntity entity in entities)
-           //entity.CreatedDate = DateTime.UtcNow;
+           entity.CreatedDate = DateTime.UtcNow;
+
         Context.AddRange(entities);
         Context.SaveChanges();
+
         return entities;
     }
 
     public TEntity Update(TEntity entity)
     {
-       // entity.UpdatedDate = DateTime.UtcNow;
+        entity.UpdatedDate = DateTime.UtcNow;
+
         Context.Update(entity);
         Context.SaveChanges();
+
         return entity;
     }
 
     public ICollection<TEntity> UpdateRange(ICollection<TEntity> entities)
     {
         foreach (TEntity entity in entities)
-         //   entity.UpdatedDate = DateTime.UtcNow;
+          entity.UpdatedDate = DateTime.UtcNow;
+
         Context.UpdateRange(entities);
         Context.SaveChanges();
+
         return entities;
     }
 
@@ -192,6 +209,7 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
     {
         SetEntityAsDeleted(entity, permanent);
         Context.SaveChanges();
+
         return entity;
     }
 
@@ -199,6 +217,7 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
     {
         SetEntityAsDeleted(entities, permanent);
         Context.SaveChanges();
+
         return entities;
     }
 
@@ -282,7 +301,7 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
         if (!permanent)
         {
             CheckHasEntityHaveOneToOneRelation(entity);
-            //await setEntityAsSoftDeletedAsync(entity);
+            await setEntityAsSoftDeletedAsync(entity);
         }
         else
         {
